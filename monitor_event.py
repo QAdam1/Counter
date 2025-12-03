86b532600c73cf520434763951290db2a9455de6
QAdam1 <151572570+QAdam1@users.noreply.github.com>
2025-12-03 18:48:52 +0000

    adjust

--- monitor_event.py
+++ monitor_event.py
@@ -74,7 +74,7 @@ def monitor_page():
                     EC.visibility_of_element_located((By.XPATH, f"//*[contains(text(), '{MESSAGE}')]"))
                 )
                 logging.info(f"{datetime.now()}: Message found. Checking again in 30 sec.")
-                send_email()
+                send_emails()
                 break
             except:
                 logging.info(f"{datetime.now()}: Message not found! Sending email notification.")
